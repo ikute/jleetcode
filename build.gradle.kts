976fb7226e2021d02570eb892ec3b17b378faa2f
ikute <ikutuzov.dev@gmail.com>
2025-03-31 19:48:54 +0700

    setup java project

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("java")
+    id("java-library")
+}
+
+group = "dev.ikute.jleetcode"
+version = "0.0.1"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+dependencies {
+
+}
